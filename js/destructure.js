14bee365a15fc6344fd841e0c29d963e8259eaab
Anshul Shrivastava <anshul.shrivastava@etelligens.in>
2023-04-04 17:35:23 +0530

    JS Refresher done

---
+++ js/destructure.js
@@ -0,0 +1,26 @@
+const person = {
+    name: 'Anshul',
+    age: 26,
+    greet() {
+      console.log('Hi, I am ' + this.name);
+    }
+  };
+  
+  const printName = ({ name }) => {
+    console.log(name);
+  };
+  
+  printName(person);
+  
+  const { name, age } = person;
+  console.log(name, age);
+  
+  // const copiedPerson = { ...person };
+  // console.log(copiedPerson);
+  
+  const hobbies = ['Sports', 'Cooking'];
+  const [hobby1, hobby2] = hobbies;
+  console.log(hobby1, hobby2);
+  
+
+  
